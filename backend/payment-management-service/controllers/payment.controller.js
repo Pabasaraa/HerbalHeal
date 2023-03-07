a253e4d19038b88e9d9b16c4858b1b2faac5eec4
Pabasaraa <pabasara.was@gmail.com>
2023-03-07 12:25:40 +0530

    Added getPayment and deletePayment functions and handled edge cases

--- backend/payment-management-service/controllers/payment.controller.js
+++ backend/payment-management-service/controllers/payment.controller.js
@@ -4,6 +4,10 @@ import paymentValidation from "../services/validation.service.js";
 
 const createPayment = async (req, res) => {
   try {
+    if (!req.body.token) {
+      throw new Error("No token provided!");
+    }
+
     try {
       const response = await axios.post(
         "http://localhost:8000/user/validatetoken",
@@ -14,7 +18,6 @@ const createPayment = async (req, res) => {
           },
         }
       );
-      console.log(response.data.data._id);
       req.body.userId = response.data.data._id;
     } catch (error) {
       throw new Error("Error while getting the user ID: " + error);
@@ -41,6 +44,79 @@ const createPayment = async (req, res) => {
   }
 };
 
+const getPaymentByUserId = async (req, res) => {
+  try {
+    if (!req.body.token) {
+      throw new Error("No token provided!");
+    }
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/user/validatetoken",
+        {},
+        {
+          headers: {
+            "x-access-token": req.body.token,
+          },
+        }
+      );
+      req.body.userId = response.data.data._id;
+    } catch (error) {
+      throw new Error("Error while getting the user ID: " + error);
+    }
+
+    const payment = await paymentService.getPaymentByUserId(req.body.userId);
+
+    res.status(200).json({
+      status: "success",
+      message: "Payment method retrieved successfully!",
+      data: payment,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+const deletePaymentByUserId = async (req, res) => {
+  try {
+    if (!req.body.token) {
+      throw new Error("No token provided!");
+    }
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/user/validatetoken",
+        {},
+        {
+          headers: {
+            "x-access-token": req.body.token,
+          },
+        }
+      );
+      req.body.userId = response.data.data._id;
+    } catch (error) {
+      throw new Error("Error while getting the user ID: " + error);
+    }
+
+    await paymentService.deletePaymentByUserId(req.body.userId);
+
+    res.status(200).json({
+      status: "success",
+      message: "Payment method deleted successfully!",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
 export default {
   createPayment,
+  getPaymentByUserId,
+  deletePaymentByUserId,
 };
--- backend/payment-management-service/routes/payment.route.js
+++ backend/payment-management-service/routes/payment.route.js
@@ -4,5 +4,8 @@ import paymentController from "../controllers/payment.controller.js";
 const router = express.Router();
 
 router.route("/new").post(paymentController.createPayment);
+router.route("/get").get(paymentController.getPaymentByUserId);
+router.route("/delete").delete(paymentController.deletePaymentByUserId);
+// TODO: Add update payment method & payment processing functionality
 
 export default router;
